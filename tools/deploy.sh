185326412402c3686eccac6708fc1826a4c9e694
rok21 <rokasgrikietis@gmail.com>
2016-12-10 16:26:02 +0100

    deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -10,19 +10,12 @@ git clone git@github.com:rok21/kc_web.git
 cd kc_web
 ~/Public/activator-dist-1.3.12/bin/activator dist
 
-
-scp -i ~/Downloads/testKey1.pem remotescript.sh ubuntu@ec2-54-229-142-68.eu-west-1.compute.amazonaws.com:~/
-echo "remotescript.sh copied"
-sleep 2
-
-ssh -i ~/Downloads/testKey1.pem ubuntu@ec2-54-229-142-68.eu-west-1.compute.amazonaws.com << EOF
+ssh -i ~/.ssh/newkey.pem ubuntu@ec2-54-171-173-163.eu-west-1.compute.amazonaws.com << EOF
 {
  sleep 2
  echo "trying kill existing process"
  
- chmod +x remotescript.sh
- ./remotescript.sh
- rm remotescript.sh
+ sudo fuser 80/tcp
  
  rm -r -f /home/ubuntu/kc_web-1.0
  rm /home/ubuntu/kc_web-1.0.zip
@@ -33,12 +26,12 @@ ssh -i ~/Downloads/testKey1.pem ubuntu@ec2-54-229-142-68.eu-west-1.compute.amazo
 } 
 EOF
 
-scp -i ~/Downloads/testKey1.pem target/universal/kc_web-1.0.zip ubuntu@ec2-54-229-142-68.eu-west-1.compute.amazonaws.com:~/
+scp -i ~/.ssh/newkey.pem target/universal/kc_web-1.0.zip ubuntu@ec2-54-171-173-163.eu-west-1.compute.amazonaws.com:~/
 
-ssh -i ~/Downloads/testKey1.pem ubuntu@ec2-54-229-142-68.eu-west-1.compute.amazonaws.com << EOF
+ssh -i ~/.ssh/newkey.pem ubuntu@ec2-54-171-173-163.eu-west-1.compute.amazonaws.com << EOF
  echo "starting the server.."
  unzip kc_web-1.0.zip
- ./kc_web-1.0/bin/kc_web >> weblog &
+ sudo ./kc_web-1.0/bin/kc_web -Dhttp.port=80 >> weblog &
  logout
 EOF
 
@@ -46,4 +39,4 @@ cd ..
 cd ..
 rm -r -f tmp
 
-echo "all done go see http://54.229.142.68:9000"
+echo "all done go see http://54.171.173.163"
--- tools/remotescript.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-kill $(cat /home/ubuntu/kc_web-1.0/RUNNING_PID)
-rm /home/ubuntu/kc_web-1.0/RUNNING_PID
